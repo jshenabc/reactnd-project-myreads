1b3c34a6dd99b4c442254ded9816d43af2cc0a6b
shenjia <jshenabc@gmail.com>
2018-05-19 01:05:40 -0400

    add searchbook component and configure route

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,6 +2,7 @@ import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import '../App.css'
 import BookShelf from './BookShelf.js'
+import { Link } from 'react-router-dom'
 
 class Landing extends React.Component {
 
@@ -39,7 +40,7 @@ class Landing extends React.Component {
             </div>
           </div>
           <div className="open-search">
-            <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+            <Link to='/search'>Add a book</Link>
           </div>
         </div>
       </div>
