a415137dfe71bf706adebc82cd7d2c9a5b8292b9
Di Zhang <Di.Zhang1@ibm.com>
2018-05-23 22:34:23 -0400

    Fix dropdown in search page

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -5,14 +5,13 @@ import Book from './Book'
 
 class BookShelf extends React.Component {
   render() {
-    const { books } = this.props
-
+    const { books, shelfedBooks } = this.props
     return (
       <div className="bookshelf-books">
         <ol className="books-grid">
       { typeof(books) === 'undefined' || books.error === 'empty query' ? <p>No result</p> : books.map(book => {
         return <li key={book.id} >
-                <Book book={book}  shelf={book.shelf} updateBook={this.props.updateBook}/>
+                <Book book={book}  shelf={book.shelf} shelfedBooks={shelfedBooks} updateBook={this.props.updateBook}/>
               </li>
       })}
         </ol>
